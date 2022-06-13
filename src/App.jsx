740a7201bf28bc837002e4caf771edb4c005386a
Jungjaeyeong <jjy8037@gmail.com>
2022-06-13 19:56:39 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter } from 'react-router-dom'
 import styled, { ThemeProvider } from 'styled-components'
 
 import AppLayout from '@/components/AppLayout'
-import GlobalStyled from '@/styles/Globalstyled'
-import theme from '@/styles/theme'
+import GlobalStyled from './styles/Globalstyled'
+import theme from './styles/theme'
 import RouterConfig from './router'
 
 const Container = styled.div`
