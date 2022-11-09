11468e52d2645888fdf0bee5f8a50db660ec3ac0
Jungjaeyeong <jjy8037@gmail.com>
2022-11-09 14:03:53 +0900

    test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,7 @@ import { ProfileContainer } from './Profile.styled'
 const Profile = () => {
 	return (
 		<ProfileContainer>
+			<div className="profile__img " />
 			<h2 className="profile__name">정재영</h2>
 			<span className="profile__fe">Frontend Developer</span>
 			<span>jjy8037@gmail.com</span>
