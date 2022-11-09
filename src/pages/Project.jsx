42bd01c04f5fd29b752469388e1533923c0e088b
Jungjaeyeong <jjy8037@gmail.com>
2022-11-09 13:53:28 +0900

    test

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -19,44 +19,44 @@ const Project = () => {
 		projectItem.findIndex(item => item.id === projectId)
 
 	return (
-		<ProjectContainer>
-			<Grid>
-				{projectItem.map(item => (
-					<Item
-						key={item.id}
-						layoutId={`box-${item.id}`}
-						onClick={onChangeProjectId(item.id)}
-					>
-						<motion.img src={item.img} layoutId={`img-${item.id}`} />
-						<motion.span layoutId={`title-${item.id}`}></motion.span>
-					</Item>
-				))}
-			</Grid>
+		// <ProjectContainer>
+		// 	<Grid>
+		// 		{projectItem.map(item => (
+		// 			<Item
+		// 				key={item.id}
+		// 				layoutId={`box-${item.id}`}
+		// 				onClick={onChangeProjectId(item.id)}
+		// 			>
+		// 				<motion.img src={item.img} layoutId={`img-${item.id}`} />
+		// 				<motion.span layoutId={`title-${item.id}`}></motion.span>
+		// 			</Item>
+		// 		))}
+		// 	</Grid>
 
-			<AnimatePresence>
-				{projectId ? (
-					<Overlay
-						initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-						animate={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
-						exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-						onClick={onChangeProjectId(null)}
-					>
-						<ActiveItem layoutId={`box-${projectId}`} className="box">
-							<motion.img
-								src={projectItem[projectItemCurrentIndex(projectId)].img}
-								layoutId={`img-${projectId}`}
-							/>
-							<motion.span layoutId={`title-${projectId}`}>
-								{projectItem[projectItemCurrentIndex(projectId)].title} <br />
-								{projectItem[projectItemCurrentIndex(projectId)].content}
-								<br />
-								{projectItem[projectItemCurrentIndex(projectId)].info}
-							</motion.span>
-						</ActiveItem>
-					</Overlay>
-				) : null}
-			</AnimatePresence>
-		</ProjectContainer>
+		// 	<AnimatePresence>
+		// 		{projectId ? (
+		// 			<Overlay
+		// 				initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+		// 				animate={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
+		// 				exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+		// 				onClick={onChangeProjectId(null)}
+		// 			>
+		// 				<ActiveItem layoutId={`box-${projectId}`} className="box">
+		// 					<motion.img
+		// 						src={projectItem[projectItemCurrentIndex(projectId)].img}
+		// 						layoutId={`img-${projectId}`}
+		// 					/>
+		// 					<motion.span layoutId={`title-${projectId}`}>
+		// 						{projectItem[projectItemCurrentIndex(projectId)].title} <br />
+		// 						{projectItem[projectItemCurrentIndex(projectId)].content}
+		// 						<br />
+		// 						{projectItem[projectItemCurrentIndex(projectId)].info}
+		// 					</motion.span>
+		// 				</ActiveItem>
+		// 			</Overlay>
+		// 		) : null}
+		// 	</AnimatePresence>
+		// </ProjectContainer>
 	)
 }
 
--- src/router.jsx
+++ src/router.jsx
@@ -3,13 +3,11 @@ import { useRoutes } from 'react-router-dom'
 
 const Home = lazy(() => import('./pages/Home'))
 const Project = lazy(() => import('./pages/Project'))
-
 const NotFound = lazy(() => import('./pages/NotFound'))
 
 const RouterConfig = () => {
 	const routes = useRoutes([
 		{ path: '/', element: <Home /> },
-
 		{ path: '/project', element: <Project /> },
 		{ path: '*', element: <NotFound /> }
 	])
